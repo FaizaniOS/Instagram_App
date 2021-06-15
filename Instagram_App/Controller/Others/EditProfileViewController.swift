f3e452b9fce259bb16c94b594f55141dcd7877d0
miz20@faizanvlog <miz20faizan@gmail.com>
2021-06-15 16:54:12 +0530

    First Commit

---
+++ Instagram_App/Controller/Others/EditProfileViewController.swift
@@ -0,0 +1,30 @@
+//
+//  EditProfileViewController.swift
+//  Instagram_App
+//
+//  Created by ZMQ MAC3 on 15/06/21.
+//  Copyright © 2021 ZMQ MAC3. All rights reserved.
+//
+
+import UIKit
+
+class EditProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
